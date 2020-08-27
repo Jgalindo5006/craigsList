064f7130879ad661063d728a7c13ad0bb9255b2c
jimmy galindo <jgalindo5006@gmail.com>
2020-08-27 14:08:38 +0930

    Modifications on setting, procfile, base.html

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -22,10 +22,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = '4u_fksr6$1$0&(&!vrdd_5$kz!js5tq(q32lxboa(vjy1db&)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
